398ec008f82925d36564f41eb483afb5549d398a
Aday <gaipoaday@gmail.com>
2018-03-21 14:32:05 +0800

    3/21

--- app/src/main/java/com/zachary/myfirstapp/AActivity.java
+++ app/src/main/java/com/zachary/myfirstapp/AActivity.java
@@ -6,15 +6,22 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class AActivity extends AppCompatActivity {
      private Notification.Builder builder;
      private NotificationManager manager;
+     public Spinner sp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_a);
+
+
+
 
 
         Button btn_B = findViewById(R.id.btn_B);
@@ -41,10 +48,24 @@ public class AActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 manager.notify(1001,builder.build());
+                sp = (Spinner) findViewById(R.id.spinner);
+                sp.setOnItemSelectedListener(spn);
             }
         });
 
 
 
     }
+    private AdapterView.OnItemSelectedListener spn = new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+            Toast toast =Toast.makeText(adapterView.getContext(),adapterView.getItemAtPosition(i).toString(),Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> adapterView) {
+
+        }
+    };
 }
