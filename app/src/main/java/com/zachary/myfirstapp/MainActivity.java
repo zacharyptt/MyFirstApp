d431d619838a892233a81f82a011efb1fd06dfd9
zacharyptt <d0280137@mail.fcu.edu.tw>
2018-05-08 18:48:43 +0800

    5/8

--- app/src/main/java/com/zachary/myfirstapp/MainActivity.java
+++ app/src/main/java/com/zachary/myfirstapp/MainActivity.java
@@ -1,10 +1,9 @@
 package com.zachary.myfirstapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,23 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        Button btn_A =findViewById(R.id.btn_A);
-        btn_A.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent  intent = new Intent(MainActivity.this,AActivity.class);
-                startActivity(intent);
-            }
-        });
-        Button btn_B =findViewById(R.id.btn_B);
-        btn_B.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent  intent = new Intent(MainActivity.this,BActivity.class);
-                startActivity(intent);
-            }
-        });
+    public void buttonClick(View view) {
+        Intent intent = new Intent();
+        intent.setClass(MainActivity.this , Page2.class);
+        startActivity(intent);
     }
 
     public void onClick(View view) {
